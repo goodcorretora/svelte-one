b085536f237d714477b214e86778737de041cba1
goodcorretora <goodcorretora@gmail.com>
2021-10-25 09:23:47 -0300

    Create Project
    First Svelte project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import App from './App.svelte';
 
+import './../node_modules/bootstrap/dist/css/bootstrap.css'
+
 const app = new App({
 	target: document.body,
-	props: {
-		name: 'world'
-	}
+	
 });
 
 export default app;
